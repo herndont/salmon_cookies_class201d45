dd9e9e67735142747fc5eb963a153e191ebe295e
herndont <herndontrey@gmail.com>
2019-02-12 22:40:32 -0600

    lab 6 assignment complete

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ var seatacAndAirport = {
   average_cookies_per_cust: 1.2,
 };
 
-var seattleCenter = {
+/*var seattleCenter = {
   location: 'Seattle Center',
   min_customers_per_hr: 11,
   max_customers_per_hr: 38,
@@ -34,12 +34,12 @@ var alki = {
   max_customers_per_hr: 13,
   average_cookies_per_cust: 4.6,
 };
-
+*/
 // by the hour, random number of customers
 // multiply customers by average number of cookies and round up with final number
 // keep a running total per store of number of cookies sold.
 
-var stores = [firstAndPike, seatacAndAirport, seattleCenter, capitolHill, alki];
+var stores = [firstAndPike, seatacAndAirport/*, seattleCenter, capitolHill, alki*/];
 
 function cookiesPerHourRnd(store){
   var num_of_cust_per_hr = Math.floor(Math.random() * (store.max_customers_per_hr - store.min_customers_per_hr + 1));
@@ -47,7 +47,12 @@ function cookiesPerHourRnd(store){
   var totalCookies_per_hr_rounded = Math.ceil(num_of_cookies_per_hr);
 
   //above is the total cookies per hour rounded up to the next cookie.
-  console.log(num_of_cust_per_hr+ ' came and ' +totalCookies_per_hr_rounded+ ' is how many cookies were sold.');
+
+
+  var myElement2 = document.getElementById('patHeader');
+  var li_el1 = document.createElement('li');
+  li_el1.textContent =((i) + ':00  ') + totalCookies_per_hr_rounded;
+  myElement2.appendChild(li_el1);
 
   return totalCookies_per_hr_rounded;
 }
@@ -56,11 +61,19 @@ for (var j = 0; j < stores.length; j++ ){
   var store = stores[j];
   var total = 0;
 
-  console.log(store.location);
+
+  var myElement = document.getElementById('patHeader');
+  var h2_el = document.createElement('h2');
+  h2_el.textContent = store.location;
+  myElement.appendChild(h2_el);
 
   for (var i=6; i < 20; i++){
     total = total + cookiesPerHourRnd(store);
   }
-  console.log(total);
+
+  var myElement1 = document.getElementById('patHeader');
+  var li_el15 = document.createElement('li');
+  li_el15.textContent = 'Closing Total' + total;
+  myElement1.appendChild(li_el15);
 }
 
